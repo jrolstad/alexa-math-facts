5f15365065a2271fc27fe835b9494433b646efa5
Josh Rolstad <jrolstad@gmail.com>
2018-03-22 20:01:34 -0700

    (@jrolstad) Refactoring to Azure Function so the code is simpler and has fewer moving parts

---
+++ alexa_math_facts_functions/mathfactsfunction.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using alexamathfunctions.application;
+
+namespace alexa_math_facts_functions
+{
+    public class mathfactsfunction
+    {
+        [FunctionName("AlexaMathFacts")]
+
+        public static HttpResponseMessage Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "AlexaMathFacts")]
+            HttpRequestMessage req,
+            TraceWriter log)
+        {
+
+            var requestData = req.Content.ReadAsAsync<AlexaServiceRequest>().Result;
+
+            var intentName = requestData?.request?.intent?.name;
+            var outputSpeech = GetSpeechResponse(intentName);
+            var response = AlexaServiceResponse.CreateOutputSpeechResponse(intentName, outputSpeech);
+
+            return req.CreateResponse(HttpStatusCode.OK, response);
+        }
+    }
+}
